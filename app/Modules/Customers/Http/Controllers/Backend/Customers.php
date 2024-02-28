5bd7015ebf35f59f285e9f8e3e33a08f15dbbc00
Eman Zaki <88564710+emanzaki@users.noreply.github.com>
2024-02-28 05:35:26 +0200

    add your own controllers

---
+++ app/Modules/Customers/Http/Controllers/Backend/Customers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Customers\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Customers extends Controller
+{
+    public function index(){
+        dd('customers Backend');
+    }
+    
+}
