43d8bfbbcfe2196fd4058af46a20cb2ae45c666e
Eman Zaki <88564710+emanzaki@users.noreply.github.com>
2024-02-29 08:13:04 +0200

    load requests

--- app/Modules/Customers/Http/Controllers/Backend/Customers.php
+++ app/Modules/Customers/Http/Controllers/Backend/Customers.php
@@ -3,11 +3,12 @@
 namespace Customers\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use Customers\Http\Requests\Backend\Store;
 use Illuminate\Http\Request;
 
 class Customers extends Controller
 {
-    public function index(){
+    public function index(Store $request){
         return view('customers::backend.index');
     }
     
